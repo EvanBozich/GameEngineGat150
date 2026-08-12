bf5ce2173a195f53319840c6e1240fc6acf5ca17
Evan Bozich <evanbozcih52@gmail.com>
2026-08-12 08:57:15 -0600

    Made the game playable again

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -18,44 +18,6 @@ int main()
 
     nu::SetWorkingDirectory("assets");
     //Initialize
-
-    // load the json data from a file
-    std::string buffer;
-    if (ReadTextFile("data/data.json", buffer))
-    {
-        // show the contents of the json file (debug)
-        std::cout << buffer << std::endl;
-
-        // create json document from the json file contents
-        rapidjson::Document document;
-        if (json::Load("data/data.json", document))
-        {
-            // read/show the data from the json file
-            std::string name;
-            int age;
-            float speed;
-            bool isAwake;
-            nu::Vector2 position;
-            nu::Vector3 color;
-
-            // read the json data
-            JSON_READ(document, name);
-            JSON_READ(document, age);
-            JSON_READ(document, speed);
-            JSON_READ(document, isAwake);
-            JSON_READ(document, position);
-            JSON_READ(document, color);
-
-            // show the data
-            std::cout << name << " " << age << " " << speed << " " << isAwake << std::endl;
-            std::cout << position.x << " " << position.y << std::endl;
-            std::cout << color.r << " " << color.g << " " << color.b << " " << std::endl;
-
-
-        }
-    }
-
-    return 0;
    
     Engine::Get().Initialize();
 
